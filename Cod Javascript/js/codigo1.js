3137c5fad3498de137fe26852006342b71c675be
Lissette Salazar <j.salaz87@gmail.com>
2024-10-21 20:43:41 -0400

    Clase 2

---
+++ Cod Javascript/js/codigo1.js
@@ -0,0 +1,9 @@
+/*alert('Hola mundo desde JavaScript')*/
+
+let nombre = prompt('Ingrese su nombre')
+if (nombre == 'root' ) {
+    alert('Acceso correcto')
+}
+else{
+    alert('Acceso denegado')
+}
